25a0016d7b1002e2133aa75c28b80e549f7e84f1
Soong En Wong <wsoongen2015@gmail.com>
2025-07-07 11:50:01 +0100

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by Soong En Wong on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnchorTests/AnchorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorTests.swift
+//  AnchorTests
+//
+//  Created by Soong En Wong on 07/07/2025.
+//
+
+import Testing
+@testable import Anchor
+
+struct AnchorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
